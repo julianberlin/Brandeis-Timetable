a74da78b885b1acfeecf7d76c6734b658da45061
Julian Berlin <julianb@brandeis.edu>
2020-06-25 13:46:03 -0400

    added admins

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const express = require("express"),
   app = express(),
   homeController = require("./controllers/homeController"),
   errorController = require("./controllers/errorController"),
-  layouts = require("express-ejs-layouts");
-
+  layouts = require("express-ejs-layouts"),
+  Admin = require("./models/Admin");
 // connect with mongoose
 const mongoose = require("mongoose");
 mongoose.connect(
@@ -48,6 +48,23 @@ app.get("/test_class_schedule",homeController.showTestSchedule);
 app.get("/profile", homeController.showProfile);
 app.get("/pro", homeController.showSchedule);
 //app.post("/contact", homeController.postedSignUpForm);
+
+//
+app.get("/addAdmins", async(req,res,next) => {
+  try{
+    await Admin.deleteMany({});
+    let user = new Admin({googleemail:'julianb@brandeis.edu'});
+    await user.save();
+    user = new Admin({googleemail:'chena@brandeis.edu'});
+    await user.save();
+    user = new Admin({googleemail:'tianqizhao@brandeis.edu'});
+    await user.save();
+    res.json("success");
+  }
+  catch(e){
+    next(e);
+  }
+})
 
 const Contact=require("./models/Contact");
 app.get("/showContacts",
