7161877765707c4610f68b80d795c417b4bc718b
Louis Zhao <louiszhao0517@yahoo.com>
2020-06-30 03:00:18 -0400

    Create envoy.sh

---
+++ envoy.sh
@@ -0,0 +1,4 @@
+$Env:clientID="80049277833-49k14a8htsajq7q8b1gafs8ahq28796c.apps.googleusercontent.com"
+$Env:clientSecret="03O8Jv4nyzLfLivc6_g-pbqF"
+$Env:callbackURL="http://localhost:5500/login/authorized"
+$Env:MONGODB_URI="mongodb://localhost/timetable"
